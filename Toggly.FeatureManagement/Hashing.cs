ae41aa07ba67218e98084fff7bb7793953e68bdd
Alexandru Puiu <alex@ops.ai>
2021-12-07 23:47:43 -0500

    Added app key hashing logic to feature provider

---
+++ Toggly.FeatureManagement/Hashing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Toggly.FeatureManagement
+{
+    internal class Hashing
+    {
+        public static string GetStringSha256Hash(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return string.Empty;
+
+            using (var sha = SHA256.Create())
+            {
+                var textData = System.Text.Encoding.UTF8.GetBytes(text);
+                var hash = sha.ComputeHash(textData);
+                return BitConverter.ToString(hash).Replace("-", string.Empty);
+            }
+        }
+    }
+}
