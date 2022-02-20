3ac6ebfc2db65911e329959036d914c695003c48
Alexandru Puiu <alex@ops.ai>
2022-02-20 15:05:46 -0500

    Added feature manager wrapper Added interface for feature usage stats Added injection decorator pattern helpers

---
+++ Toggly.FeatureManagement/IFeatureUsageStatsProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Toggly.FeatureManagement
+{
+    public interface IFeatureUsageStatsProvider
+    {
+        Task RecordUsageAsync(string feature, bool allowed);
+
+        Task RecordUsageAsync<TContext>(string feature, TContext context, bool allowed);
+    }
+}
---
+++ Toggly.FeatureManagement/TogglyFeatureManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.FeatureManagement;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Toggly.FeatureManagement
+{
+    public class TogglyFeatureManager : IFeatureManager
+    {
+        private readonly IFeatureManager _featureManager;
+
+        private readonly IFeatureUsageStatsProvider _featureUsageStatsProvider;
+
+        public TogglyFeatureManager(IFeatureManager featureManager, IFeatureUsageStatsProvider featureUsageStatsProvider)
+        {
+            _featureManager = featureManager;
+            _featureUsageStatsProvider = featureUsageStatsProvider;
+        }
+
+        public IAsyncEnumerable<string> GetFeatureNamesAsync() => _featureManager.GetFeatureNamesAsync();
+
+        public async Task<bool> IsEnabledAsync(string feature)
+        {
+            bool allowed = await _featureManager.IsEnabledAsync(feature);
+
+            //Notify usage stats service that it was checked
+            await _featureUsageStatsProvider.RecordUsageAsync(feature, allowed);
+
+            return allowed;
+        }
+
+        public async Task<bool> IsEnabledAsync<TContext>(string feature, TContext context)
+        {
+            bool allowed = await _featureManager.IsEnabledAsync(feature, context);
+
+            //Notify usage stats service that it was checked
+            await _featureUsageStatsProvider.RecordUsageAsync(feature, context, allowed);
+
+            return allowed;
+        }
+    }
+}
