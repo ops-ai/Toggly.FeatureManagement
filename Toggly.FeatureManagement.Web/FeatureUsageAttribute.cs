c3556b85765637cfcdaf7f261e0ad3677f249fe8
Alexandru Puiu <alex@ops.ai>
2022-05-02 22:53:20 -0400

    Added FeatureUsage attribute to simplify reporting feature usage

---
+++ Toggly.FeatureManagement.Web/FeatureUsageAttribute.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Toggly.FeatureManagement
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    public class FeatureUsageAttribute : ActionFilterAttribute, IAsyncPageFilter
+    {
+        /// <summary>
+        /// Creates an attribute that signals a feature was used.
+        /// </summary>
+        /// <param name="features">The names of the features that the attribute will represent.</param>
+        public FeatureUsageAttribute(params string[] features)
+        {
+            if (features == null || features.Length == 0)
+                throw new ArgumentNullException(nameof(features));
+
+            Features = features;
+        }
+
+        /// <summary>
+        /// Creates an attribute that signals a feature was used.
+        /// </summary>
+        /// <param name="features">A set of enums representing the features that the attribute will represent.</param>
+        public FeatureUsageAttribute(params object[] features)
+        {
+            if (features == null || features.Length == 0)
+                throw new ArgumentNullException(nameof(features));
+
+            var fs = new List<string>();
+
+            foreach (object feature in features)
+            {
+                var type = feature.GetType();
+
+                if (!type.IsEnum)
+                {
+                    // invalid
+                    throw new ArgumentException("The provided features must be enums.", nameof(features));
+                }
+
+                fs.Add(Enum.GetName(feature.GetType(), feature));
+            }
+
+            Features = fs;
+        }
+
+        /// <summary>
+        /// The name of the features that the feature attribute will activate for.
+        /// </summary>
+        public IEnumerable<string> Features { get; }
+
+        /// <summary>
+        /// Performs controller action pre-procesing to ensure that at least one of the specified features are enabled.
+        /// </summary>
+        /// <param name="context">The context of the MVC action.</param>
+        /// <param name="next">The action delegate.</param>
+        /// <returns>Returns a task representing the action execution unit of work.</returns>
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            IFeatureUsageStatsProvider statsProvider = context.HttpContext.RequestServices.GetRequiredService<IFeatureUsageStatsProvider>();
+
+            foreach (var feature in Features)
+                await statsProvider.RecordUsageAsync(feature);
+
+            await next().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Called asynchronously before the handler method is invoked, after model binding is complete.
+        /// </summary>
+        /// <param name="context">The <see cref="PageHandlerExecutingContext"/>.</param>
+        /// <param name="next">The <see cref="PageHandlerExecutionDelegate"/>. Invoked to execute the next page filter or the handler method itself.</param>
+        /// <returns>A <see cref="Task"/> that on completion indicates the filter has executed.</returns>
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            IFeatureUsageStatsProvider statsProvider = context.HttpContext.RequestServices.GetRequiredService<IFeatureUsageStatsProvider>();
+
+            foreach (var feature in Features)
+                await statsProvider.RecordUsageAsync(feature);
+
+            await next.Invoke().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Called asynchronously after the handler method has been selected, but before model binding occurs.
+        /// </summary>
+        /// <param name="context">The <see cref="PageHandlerSelectedContext"/>.</param>
+        /// <returns>A <see cref="Task"/> that on completion indicates the filter has executed.</returns>
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context) => Task.CompletedTask;
+    }
+}
