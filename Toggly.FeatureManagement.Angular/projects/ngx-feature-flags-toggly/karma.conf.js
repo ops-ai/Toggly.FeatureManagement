8f210e0274ebd9f04ee9ed397377847fc77e3379
Alexandru Puiu <alex@ops.ai>
2026-02-25 19:32:36 -0600

    Fixed smoke test Karma failure when no app key is configured by setting failOnEmptyTestSuite to false

--- Toggly.FeatureManagement.Angular/projects/ngx-feature-flags-toggly/karma.conf.js
+++ Toggly.FeatureManagement.Angular/projects/ngx-feature-flags-toggly/karma.conf.js
@@ -49,6 +49,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    failOnEmptyTestSuite: false
   });
 };
