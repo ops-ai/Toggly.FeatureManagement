3e39ee2bb8968ad9526233fa67c107a310a43a92
Alexandru Puiu <alex@ops.ai>
2026-09-22 11:20:15 -0500

    Release Android Maven Central Portal deployments automatically [OPS-1353]
    Uploads succeeded but never reached repo1 because vanniktech published
    without automaticRelease (unlike Java autoPublish=true). Enable
    automaticRelease, use publishAndReleaseToMavenCentral, and resolve
    versions against Maven Central instead of git tags alone.
    
    Linear Issues:
    - OPS-1353: Fix Android SDK Maven Central Portal stuck deploy

--- Toggly.FeatureManagement.Android/toggly-android-core/build.gradle.kts
+++ Toggly.FeatureManagement.Android/toggly-android-core/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
 }
 
 mavenPublishing {
-    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
     signAllPublications()
 
     coordinates("io.toggly", "toggly-android-core", version.toString())
--- Toggly.FeatureManagement.Android/toggly-datastore/build.gradle.kts
+++ Toggly.FeatureManagement.Android/toggly-datastore/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
 }
 
 mavenPublishing {
-    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
     signAllPublications()
 
     coordinates("io.toggly", "toggly-datastore", version.toString())
